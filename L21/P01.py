f839cf4aace496f77c4a31420cd2e818e1e2a48a
CatBrainJordanABaumann <bbaumann2194@floridapoly.edu>
2026-03-11 18:43:02 +0000

    Stuff as of class of 3/111/26

---
+++ L21/P01.py
@@ -0,0 +1,36 @@
+# numpy_random.py
+import numpy as np
+import pandas as pd
+import streamlit as st;
+
+num_rows = 5
+data = []
+for i in range(num_rows):
+    data.append(
+        {
+            "Preview": f"https://picsum.photos/400/200?lock={i}",
+            "Views": np.random.randint(0, 1000),
+            "Active": np.random.choice([True, False]),
+            "Category": np.random.choice(["🤖 LLM", "📊 Data", "⚙️ Tool"]),
+            "Progress": np.random.randint(1, 100),
+        }
+    )
+
+st.write(pd.DataFrame(data))
+num_rows = st.slider("Number of rows", 1, 10000, 500)
+np.random.seed(34)
+
+data = pd.DataFrame(data)
+
+config = {
+    "Preview": st.column_config.ImageColumn(),
+    "Progress": st.column_config.ProgressColumn(),
+}
+
+if st.toggle("Enable editing"):
+    edited_data = st.data_editor(data, column_config=config, use_container_width=True)
+else:
+    st.dataframe(data, column_config=config, use_container_width=True)
+
+st.subheader("Top 10 Views")
+st.line_chart(data["Views"].head(10))
